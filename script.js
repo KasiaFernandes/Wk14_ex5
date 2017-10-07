ecf33fbc278868596bc07d1edae759394d0f9d88
Kasia Fernandes <fernandes.kasia@gmail.com>
2017-10-07 12:03:38 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const app = (
+<div className={"star8 gold"}>
+    <i clasName={"fa fa-hand-o-up"}></i>
+</div>
+<div className={"star8 orange"}>
+    <i clasName={"fa fa-diamond"}></i>
+</div>
+<div className={"star8 blue"}>
+    <i clasName={"fa fa-flask"}></i>
+</div>); 
+
+ReactDOM.render(app, document.getElementById("app"));
